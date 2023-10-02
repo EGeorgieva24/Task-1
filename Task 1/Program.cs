5cdc84247dd524394c5db1ab807e3e1101ff0afa
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-10-02 20:41:22 +0300

    Add project files.

---
+++ Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿class Test
+{
+    public static void Main()
+    {
+        string path = @"D:\Programming guysss";
+        DirectoryInfo Directory = new DirectoryInfo(path);
+        Directory.Create();
+
+        var file = $@"{path}\ElitzaG.txt";
+
+        Console.WriteLine("Write something");
+        string write = Console.ReadLine();
+
+        
+
+        if (write.Contains("M"))
+        {
+            File.WriteAllText(file, write);
+            Console.WriteLine($"Input '{write}' written to .");
+        }
+        else
+        {
+            Console.WriteLine($"Input does not contain 'M', file remains empty.");
+        }
+        Console.WriteLine(File.GetCreationTime(file));
+        Console.WriteLine(File.GetLastAccessTime(file));
+        Console.WriteLine(Path.GetFileName(file));
+
+        Console.WriteLine(Path.GetDirectoryName(file));
+    }
+}
